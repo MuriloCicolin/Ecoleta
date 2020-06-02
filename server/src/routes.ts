7b41b79b03dff0cbae9d4e9f558f533a89560c7e
MuriloCicolin <mu_cicolin@hotmail.com>
2020-06-02 17:35:17 -0300

    initial commit

---
+++ server/src/routes.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+
+
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
+
+const routes = express.Router();
+
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+
+
+routes.get('/points/:id', pointsController.show);
+routes.get('/points', pointsController.index);
+routes.post('/points', pointsController.create);
+routes.get('/items', itemsController.index);
+
+
+export default routes;
