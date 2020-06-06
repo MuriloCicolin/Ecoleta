7cafc92e21db630577a466507f167f48bbe228eb
MuriloCicolin <mu_cicolin@hotmail.com>
2020-06-06 14:13:07 -0300

    Validacao de entrada de dados

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
+import { celebrate, Joi } from 'celebrate';
 
 
 import PointsController from './controllers/PointsController';
@@ -18,7 +19,23 @@ const itemsController = new ItemsController();
 routes.get('/points/:id', pointsController.show);
 routes.get('/points', pointsController.index);
 
-routes.post('/points',upload.single('image'), pointsController.create);
+routes.post('/points', 
+upload.single('image'), 
+celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required(),
+    whatsapp: Joi.number().required(),
+    latitude: Joi.number().required(),
+    longitude: Joi.number().required(),
+    city: Joi.string().required(),
+    uf: Joi.string().max(2),
+    items: Joi.string().required(),
+  })
+}, {
+  abortEarly: false,
+}),  
+pointsController.create);
 
 routes.get('/items', itemsController.index);
 
